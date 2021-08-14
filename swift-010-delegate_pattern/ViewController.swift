f715114c018e98f5402e54af025a2771d6c929b5
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-14 16:43:57 -0300

    Initial Commit

---
+++ swift-010-delegate_pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-010-delegate_pattern
+//
+//  Created by Luiz Carlos da Silva Araujo on 14/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
